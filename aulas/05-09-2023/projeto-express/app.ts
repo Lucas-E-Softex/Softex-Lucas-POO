a7d560c6c5b8c916387ce5f760f9047c3ae8e62b
Lucas-E-Softex <140561832+Lucas-E-Softex@users.noreply.github.com>
2023-09-05 19:58:25 -0400

    coloquei router

--- aulas/05-09-2023/projeto-express/app.ts
+++ aulas/05-09-2023/projeto-express/app.ts
@@ -3,12 +3,15 @@ import cookieParser from 'cookie-parser';
 import modelsSync from './utils/sync';
 import { sequelize } from './models/config';
 import { User } from './models/User';
+import router from './routes/user'
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 sequelize.authenticate()
 User.sync()
+
+app.use('/', router);
 
 app.listen('3000', () => {
     console.log('Conectado')
